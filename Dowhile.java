911b72926f8ff2c4215ea731f8a23b138a62ff38
Ganapathiraj <ganapathiraj810@gmail.com>
2026-07-17 15:02:32 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do {
+            System.out.println(i + " ");
+            i++; // Increment so the loop eventually ends
+        } while (i < 5);
+    }
+}
