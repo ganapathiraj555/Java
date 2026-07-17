e381420f9a0149d10334b144f66ac5db48aaac89
Ganapathiraj <ganapathiraj810@gmail.com>
2026-07-17 15:20:48 +0530

    Add files via upload

---
+++ Return.java
@@ -0,0 +1,13 @@
+public class Return {
+    public static void main(String[] args) {
+        System.out.println("Start the execution");
+        
+        int i = 0;
+        if (i == 0) {
+            System.out.println("Return executed");
+            return;
+        }
+        
+        System.out.println("Stop the execution");
+    }
+}
