0a77455dfd79dd386e0eab7de0b7c2cd1b269624
Ganapathiraj <ganapathiraj810@gmail.com>
2026-07-23 14:13:17 +0530

    Sub string siei

---
+++ Substringsiei.java
@@ -0,0 +1,8 @@
+public class Substringsiei
+{
+    public static void main (String[] args) {
+    String s = "Ramu";
+System.out.println(s.substring(0)); 
+System.out.println(s.substring(1, 4));
+    }
+}
